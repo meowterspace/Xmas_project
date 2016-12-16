64529082455088fe715be7c95debf547073197c2
Cat Dunicliff <ben@bromleycroft.co.uk>
2016-12-16 11:52:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ FLOORW = pygame.image.load('images/tilewhite.png')
 SIDE = pygame.image.load('images/tilewall.png')
 GOAL = pygame.image.load('images/goal.png')
 # BLACK = pygame.image.load('images/black.png')
-player = [pygame.image.load('images/santa.png'),]
+player = [pygame.image.load('images/santa.png'), ]
 display = (TILE_COLUMNS * MAP_WIDTH, TILE_ROWS * MAP_HEIGHT)
 fog_of_war = pygame.Surface(display)
 
@@ -90,6 +90,7 @@ delcount = 0
 gifts = ["bone", "chainsaw", "deathstar", "pizza"]
 holding = []
 inventory = []
+
 # =======================================================================================================================
 # FUNCTIONS
 # =======================================================================================================================
@@ -105,7 +106,7 @@ def readRoomsfile(filename):
 def draw_map(screen):
     for index, tile in enumerate(TILEMAP):
         idy = int(floor(index / 10) * 45)
-        idx = int((index % 10) * 45)   # TODO changed
+        idx = int((index % 10) * 45)  # TODO changed
         if tile == 0:
             screen.blit(TOP, (idx, idy))
         elif tile == 1:
@@ -138,7 +139,7 @@ def draw_map(screen):
 
 def check_block(x, y):
     print x, y
-    px = x /45
+    px = x / 45
     py = y / 45
     check_index = TILEMAP[py * 10 + px]
 
@@ -148,9 +149,12 @@ def check_block(x, y):
             print "PRESENT: " + str(i)
     if check_index == 5:
         return -5
-    elif check_index == 6: return -6
-    elif check_index == 7: return -7
-    elif check_index == 8: return -8
+    elif check_index == 6:
+        return -6
+    elif check_index == 7:
+        return -7
+    elif check_index == 8:
+        return -8
     elif check_index != 0:
         print delPres
         return -1  # True
@@ -168,10 +172,10 @@ def move(start_x, start_y, x, y):  # 25, 25
     elif check_block(end_x, end_y) >= 0:
         present(check_block(end_x, end_y), pygame.display.set_mode((MAP_WIDTH * TILE_COLUMNS, MAP_HEIGHT * TILE_ROWS)))
         return (end_x, end_y)
-    elif check_block(end_x, end_y) == -5: # TODO fix...
+    elif check_block(end_x, end_y) == -5:  # TODO fix...
         if len(inventory) > 0:
             inventory.pop(0)
-            print "delivered "+str(holding[0])+" to Festive Doggo"
+            print "delivered " + str(holding[0]) + " to Festive Doggo"
             delPeeps.append("festive doggo")
             delPres.append(str(holding[0]))
             holding.pop(0)
@@ -180,10 +184,10 @@ def move(start_x, start_y, x, y):  # 25, 25
         else:
             print "you can't deliver without a gift? What kind of santa are you?"
             return (start_x, start_y)
-    elif check_block(end_x, end_y) == -6: # TODO fix...
+    elif check_block(end_x, end_y) == -6:  # TODO fix...
         if len(inventory) > 0:
             inventory.pop(0)
-            print "delivered "+str(holding[0])+" to Billy"
+            print "delivered " + str(holding[0]) + " to Billy"
             delPeeps.append("Billy")
             delPres.append(str(holding[0]))
             holding.pop(0)
@@ -192,10 +196,10 @@ def move(start_x, start_y, x, y):  # 25, 25
         else:
             print "you can't deliver without a gift? What kind of santa are you?"
             return (start_x, start_y)
-    elif check_block(end_x, end_y) == -7: # TODO fix...
+    elif check_block(end_x, end_y) == -7:  # TODO fix...
         if len(inventory) > 0:
             inventory.pop(0)
-            print "delivered "+str(holding[0])+" to Vader"
+            print "delivered " + str(holding[0]) + " to Vader"
             delPeeps.append("Darth Vader")
             delPres.append(str(holding[0]))
             holding.pop(0)
@@ -204,10 +208,10 @@ def move(start_x, start_y, x, y):  # 25, 25
         else:
             print "you can't deliver without a gift? What kind of santa are you?"
             return (start_x, start_y)
-    elif check_block(end_x, end_y) == -8: # TODO fix...
+    elif check_block(end_x, end_y) == -8:  # TODO fix...
         if len(inventory) > 0:
             inventory.pop(0)
-            print "delivered "+str(holding[0])+" to Francine"
+            print "delivered " + str(holding[0]) + " to Francine"
             delPeeps.append("Francine")
             delPres.append(str(holding[0]))
             holding.pop(0)
@@ -227,7 +231,7 @@ def present(index, screen):
     # TODO display item
     inventory.append(index)
     holding.append(gifts[index])
-    print "inventory"+str(inventory)
+    print "inventory" + str(inventory)
     screen.blit(OBJECTS[index], (90, 90))
     print PRESPOS
 
@@ -254,14 +258,13 @@ def main():
     clock = pygame.time.Clock()
 
     for index, item in enumerate(PRESENTS):
-        ax = random.randint(2, 7) # TODO - Change ax range so it doesn't blit into a wall
-        ay = random.randint(3, 8) # TODO - Change ay range so it doesn't blit into a wall
+        ax = random.randint(2, 7)  # TODO - Change ax range so it doesn't blit into a wall
+        ay = random.randint(3, 8)  # TODO - Change ay range so it doesn't blit into a wall
         bx = ax * 45
         by = ay * 45
         PRESPOS.append([bx, by])
     print PRESPOS
     while True:
-
 
         clock.tick(30)
 
@@ -270,16 +273,15 @@ def main():
             draw_map(screen)
             for index, item in enumerate(PRESPOS):
                 screen.blit(PRESENTS[index], (PRESPOS[index][0], PRESPOS[index][1]))
-            label = FONT.render("Inventory:", 1, (0,0,0))
+            label = FONT.render("Inventory:", 1, (0, 0, 0))
             screen.blit(label, (315, 20))
             if len(inventory) > 0:
-               #screen.blit(PRESENTS[1], (450, 0)) # TODO blit presents top right
-               screen.blit(PRESENTS[1], (405, 20))
-             # pygame.draw.rect(fog_of_war, (60, 60, 60), (playerx,playery+45,50,50))
-             # fog_of_war.set_colorkey((60, 60, 60))
+                # screen.blit(PRESENTS[1], (450, 0)) # TODO blit presents top right
+                screen.blit(PRESENTS[1], (405, 20))
+                # pygame.draw.rect(fog_of_war, (60, 60, 60), (playerx,playery+45,50,50))
+                # fog_of_war.set_colorkey((60, 60, 60))
 
-             # print inventory
-
+                # print inventory
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -291,7 +293,8 @@ def main():
                     if event.key == K_ESCAPE:
                         sys.exit(0)
                     elif event.key == K_LEFT:
-                        current_x, current_y = move(current_x, current_y, -45, 0)  # LEFT = (-45,0)  RIGHT = (45, 0)  UP = (0,-45)  DOWN=(0, 45)
+                        current_x, current_y = move(current_x, current_y, -45,
+                                                    0)  # LEFT = (-45,0)  RIGHT = (45, 0)  UP = (0,-45)  DOWN=(0, 45)
 
                         update(player[0], current_x, current_y, screen)
                         player_positions[0] = [current_x, current_y]
@@ -315,28 +318,32 @@ def main():
                         player_positions[0] = [current_x, current_y]
 
         else:
-            print("It was a busy christmas for santa clause. As he lay down in bed after a hard nights work, he wondered what everyone was thinking...")
+            print(
+            "It was a busy christmas for santa clause. As he lay down in bed after a hard nights work, he wondered what everyone was thinking...")
             print("")
             print("")
-            print (len(delPres))
-            for i in range(1,4):
+            for i in range(0, len(delPres)):
                 if delPres[i] == "bone":
-                    print(delPeeps[i]+" tore off the wrapping paper, revealing a glorious shiny bone. "+delPeeps[i]+" chewed and knarled the bone all day. Truly a christmas to remember")
+                    print(delPeeps[i] + " tore off the wrapping paper, revealing a glorious shiny bone. " + delPeeps[
+                        i] + " chewed and knarled the bone all day. Truly a christmas to remember")
                 elif delPres[i] == "chainsaw":
-                    print(delPeeps[i]+" knew what it was before even unwrapping it. They had been waiting for this day their entire lives. "+delPeeps[i]+" wielded the chainsaw, as they had dreamed of many times before.")
-                    print("'Time to visit a friend' "+delPeeps[i]+" said laughing")
+                    print(delPeeps[
+                              i] + " knew what it was before even unwrapping it. They had been waiting for this day their entire lives. " +
+                          delPeeps[i] + " wielded the chainsaw, as they had dreamed of many times before.")
+                    print("'Time to visit a friend' " + delPeeps[i] + " said laughing")
                 elif delPres[i] == "deathstar":
-                    print(delPeeps[i]+" wasn't really sure what to do with their moon sized planet destroyer. ")
+                    print(delPeeps[i] + " wasn't really sure what to do with their moon sized planet destroyer. ")
                 elif delPres[i] == "pizza":
-                    print(delPeeps[i]+" stared hungrily into the now empty pizza box. 'I'm such a pig' they proclaimed. Slowly, "+delPeeps[i]+" became even more depressed")
+                    print(delPeeps[i] + " stared hungrily into the now empty pizza box. 'I'm such a pig' they proclaimed. Slowly, " +
+                          delPeeps[i] + " became even more depressed")
             print("")
             print("")
             print("")
             print ("MERRY XMAS")
             sys.exit(0)
-                    #          update(player[current_index], current_x, current_y, screen)
-                    #               current_x, current_y = player_positions[0]
-                    #               update(player[0], current_x, current_y, screen)
+            #          update(player[current_index], current_x, current_y, screen)
+            #               current_x, current_y = player_positions[0]
+            #               update(player[0], current_x, current_y, screen)
 
 
 if __name__ == '__main__':
